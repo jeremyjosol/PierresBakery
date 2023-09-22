f2812dd17b7eb7a8bfb9d8d9e9f5f0219ac0a68a
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-22 12:15:25 -0700

    Refactor PastryTests, set Croissant properties public + pass tests

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -10,18 +10,20 @@ namespace PierresBakery.Tests
     [TestMethod]
     public void CalculateTotalPastryPrice_WillUpdateTotalPastryPrice_Int()
     {
+      Croissant croissantPastry = new Croissant();
       int orderQuantity = 2;
       int expectedPrice = 4;
-      Pastry.CalculateTotalPastryPrice(orderQuantity);
-      Assert.AreEqual(expectedPrice, Pastry.TotalPastryPrice);
+      croissantPastry.CalculateTotalPastryPrice(orderQuantity);
+      Assert.AreEqual(expectedPrice, croissantPastry.TotalPastryPrice);
     }
     [TestMethod]
     public void CalculateTotalPastryPrice_WillUpdateTotalPastryPriceDiscount_Int()
     {
+      Croissant croissantPastry = new Croissant();
       int orderQuantity = 8;
       int expectedPrice = 12;
-      Pastry.CalculateTotalPastryPrice(orderQuantity);
-      Assert.AreEqual(expectedPrice, Pastry.TotalPastryPrice);
+      croissantPastry.CalculateTotalPastryPrice(orderQuantity);
+      Assert.AreEqual(expectedPrice, croissantPastry.TotalPastryPrice);
     }
   }
 }
